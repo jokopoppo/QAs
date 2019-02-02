f3373fd9a9467f7f7b79af853051d5bc6a7e6a05
wachirawit.kan@gmail.com <wachirawit.kan@gmail.com>
2019-02-02 13:31:39 +0700

    add relevance score

--- findAnswer.py
+++ findAnswer.py
@@ -1,16 +1,32 @@
 import json
 
-def answer_word(doc,answer_begin,answer_end):
-    word = ''
-    for i in range(answer_begin - 1, answer_end):
-        word +=doc[i]
-    return word
-
 def extractNumberFromString(string):
     import re
     return re.findall('\d+', string )
+
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
+
+def make_sentence_answer(doc,answer_begin,n=15):
+    sentence_answer = []
+    l = 0
+    for j in range(doc.__len__()):
+        l += doc[j].__len__()
+        if l >= answer_begin - 1:
+            for k in range(j - n, j + n):
+                try:
+                    sentence_answer.append(doc[k])
+                except IndexError:
+                    break
+            break
+    return sentence_answer
+
+def normalized_edit_similarity(a, b):
+    import editdistance
+    return 1.0 - editdistance.eval(a, b)/(1.0 * max(len(a), len(b)))
+
+def order_consistency(index_a,index_b,l):
+    return 1.0 - abs(index_a - index_b)/l
 
 a = json.load(open('test_set/new_sample_questions.json',encoding='utf-8-sig'))
 a = a['data']
@@ -19,97 +35,81 @@ question = json.load(open('test_set\\new_sample_questions_tokenize.json', 'r', e
 question_index = []
 doc_id = []
 real_answer = []
-question_words = ['กี่', 'ปี', 'เท่า' ,'ใด' , 'ไร' ,'อะไร']
-for i in range(a.__len__()):
+question_words = ['กี่', 'อะไร', 'ใด', 'เท่า', 'ปี']
+wrong = 949
+all_rs = []
+possible_answer = []
+for i in range(wrong,a.__len__()):
     article_id = a[i]['article_id']
     answer = a[i]['answer']
     answer_begin = a[i]['answer_begin_position ']
     answer_end = a[i]['answer_end_position']
 
     if answer.isnumeric():
+        s = ''.join(question[i])
+        print(i, s)
+        question_index.append(i)
         real_answer.append([article_id,answer])
         doc = json.load(open('E:\CPE#Y4\databaseTF\documents-tokenize\\'+str(article_id)+'.json','r',encoding='utf-8-sig'))
-        sentence_answer = []
-        l = 0
-        for j in range(doc.__len__()):
-            l += doc[j].__len__()
-            if l >= answer_begin - 1:
-                for k in range(j-15,j+15):
-                    try:
-                        sentence_answer.append(doc[k])
-                    except IndexError:
-                        break
 
-                doc_id.append([article_id])
-                break
+        sentence_answer = make_sentence_answer(doc,answer_begin)
+        doc_id.append([article_id])
 
-        for k in question[i]:
+        for j in question[i]:
             for w in question_words:
-                if k.endswith(w) or k.startswith(w):
-                    for j in sentence_answer:
-                        if hasNumbers(j):
-                            doc_id[-1].append(j)
+                if j.endswith(w) or j.startswith(w):
+                    # s = s.replace(w,'')
+                    for k in range(sentence_answer.__len__()):
+                        if hasNumbers(sentence_answer[k]):
+                            doc_id[-1].append(k)
                             # for num in extractNumberFromString(j):
                             #     question_index[-1].append(num)
                     break
 
         doc_id[-1][1:] = list(set(doc_id[-1][1:]))
-        question_index.append(i)
-        print(i,question[i])
         print(sentence_answer, doc_id[-1])
-exit(0)
+        if doc_id[-1].__len__() < 3:
+            all_rs.append(1)
+            possible_answer.append(sentence_answer[doc_id[-1][1]])
+            doc_id[-1].insert(1, extractNumberFromString(sentence_answer[doc_id[-1][1]])[0])
+            continue
+
+        rs = []
+        possible_answer.append([])
+        for j in doc_id[-1][1:]:
+            print(sentence_answer[j])
+            possible_answer[-1].append(sentence_answer[j])
+            score = []
+            od = []
+            for k in range(question[i].__len__()):
+                tmp = ''.join(sentence_answer[j-k:j-k + question[i].__len__()])
+                if not tmp :
+                    continue
+                od = []
+                for l in range(question[i].__len__()):
+                    try:
+                        od.append(order_consistency(sentence_answer.index(question[i][l]), l, question[i].__len__()))
+                    except ValueError:
+                        od.append(0)
+                print(normalized_edit_similarity(s, tmp),sum(od)/od.__len__())
+                score.append(normalized_edit_similarity(s, tmp) + sum(od)/od.__len__())
+            rs.append(max(score))
+        print(rs)
+        all_rs.append(rs)
+        tmp_answer = extractNumberFromString(sentence_answer[doc_id[-1][rs.index(max(rs)) + 1]])
+        doc_id[-1].insert(1,tmp_answer[0])
+        exit(0)
+
 print("Q:", doc_id.__len__(), real_answer.__len__())
-
-test1 = []
-test2 = []
-for i in real_answer:
-    test1.append(i[0])
-for i in doc_id:
-    test2.append(i[0])
-
-print(set(test1) - set(test2))
-
-from sqlitedict import SqliteDict
-import time
-
-print("Init db")
-start = time.time()
-dict = SqliteDict('E:\CPE#Y4\databaseTF\lastest_db\doc_add_missing.sqlite', autocommit=True)
-dict = dict['doc']
-print("Time to initial db", time.time() - start)
-
-check_tfidf = []
-for i in doc_id:
-    tmp = []
-    for j in i[1:]:
-        try :
-            for k in dict[j[0]][j][1:]:
-                if k[0] == str(i[0]):
-                    tmp.append(k[1])
-                    break
-                else:
-                    tmp.append(0)
-                    break
-            # if tmp.__len__() < 1 :
-            #     print(j, i[0])
-            #     print(dict[j[0]][j][1:])
-            #     exit(0)
-        except KeyError:
-            tmp.append(0)
-    check_tfidf.append(i[tmp.index(max(tmp)) + 1])
-
-
-print(real_answer.__len__())
-print(check_tfidf.__len__())
 
 string = ''
 miss = 0
 for i in range(real_answer.__len__()):
-    if real_answer[i][1] != extractNumberFromString(check_tfidf[i])[0]:
-        string += str(question_index[i]) + ' ' +str(doc_id[i][0]) + ' ' + str(real_answer[i][1]) + ' ' + extractNumberFromString(check_tfidf[i])[0] + ' ' + str(doc_id[i][1:]) + '\n'
+    if real_answer[i][1] != doc_id[i][1]:
+        string += str(question_index[i]) + ' ' + str(real_answer[i]) + ' ' + str(doc_id[i][1]) + ' ' + str(possible_answer[i]) + ' ' + str(all_rs[i]) +'\n'
         miss+=1
 print(miss)
-
-with open("result_find_answer_word.txt", "a", encoding="utf-8") as text_file:
+string += str(miss)
+with open("result_find_answer_word(1).txt", "a", encoding="utf-8") as text_file:
     text_file.write(string)
 # TODO find the way to extract the answer from sentence
--- test1.py
+++ test1.py
@@ -1,12 +1,38 @@
 import editdistance
-
+# import deepcut
 def normalized_edit_similarity(a, b):
     return 1.0 - editdistance.eval(a, b)/(1.0 * max(len(a), len(b)))
 
-s = [ 'This is red car',
- 'Red car is here',
- 'This is not a blue car',
- 'Blue or black car is here']
+def order_consistency(index_a,index_b,l):
+    return 1.0 - abs(index_a - index_b)/l
 
-for i in s :
-    print(normalized_edit_similarity('red car is this', i))
+def extractNumberFromString(string):
+    import re
+    return re.findall('\d+', string )
+
+
+def relevance_score(question,sentence,candidate,index):
+
+    a = []
+    l = 2*question.__len__()
+    for i in c :
+        a.append([])
+        for j in range(i-l,i+l):
+            if j < sentence.__len__() and sentence[j] in question:
+                a[-1].append([question.index(sentence[j]),j])
+
+    m = a.__len__()
+    score = []
+    for i in range(m) :
+        tmp = 0
+        for j in a[i]:
+            tmp += (1-abs(j[1] - candidate[i])/l)*(1-abs(j[0] - index)/m)
+        score.append(tmp)
+
+    return max(score)
+
+q = ['ผลไม้', 'ราคา', 'กี่', 'บาท']
+s = ['วัน', 'นี้', 'ตอน', 'เที่ยง', 'มา', 'นี', 'ซื้อ', 'ข้าว', 'มัน', 'ไก่', 'จาน', 'ละ', ' ', '35', ' ', 'บาท', ' ', 'กับ', 'ผลไม้', 'ถุง', 'ละ', ' ', '20', ' ', 'บาท', ' ', 'น้าชา', 'อีก', ' ', '10', ' ', 'บาท', ' ', 'รวม', 'แล้ว', 'มา', 'นี', 'ใช้', 'เงิน', 'ไป', ' ', '65', ' ', 'บาท']
+c = [13,22,29,41]
+relevance_score(q,s,c,2)
+
