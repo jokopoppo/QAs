24e2f8144c760ee9e0d023bd7879de2b89ecbd6c
wachirawit.kan@gmail.com <wachirawit.kan@gmail.com>
2019-02-05 22:15:25 +0700

    add train set for something name

--- findAnswer_other.py
+++ findAnswer_other.py
@@ -1,8 +1,5 @@
 import json
-# import warnings
-# warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
-# warnings.filterwarnings(action='ignore', category=FutureWarning)
-# from gensim.models import Word2Vec
+
 def normalized_edit_similarity(a, b):
     import editdistance
     return 1.0 - editdistance.eval(a, b)/(1.0 * max(len(a), len(b)))
@@ -38,13 +35,13 @@ question = json.load(open('test_set\\new_sample_questions_tokenize.json', 'r', e
 question_index = []
 doc_id = []
 real_answer = []
-question_words = ['กี่', 'อะไร', 'ใด', 'เท่า', 'ปี' ]
+question_words = ['กี่', 'อะไร', 'ใด', 'เท่า', 'ปี' ,'ใคร' , 'ว่า' ,'อะไร']
+classes = ['number' , 'ใคร' , 'ว่า อะไร']
 wrong = 0
 all_rs = []
 possible_answer = []
 
 n = 0
-# model = Word2Vec.load("E:\CPE#Y4\databaseTF\word2vec_model_lastest\word2vec.model")
 ans = []
 s_ans = []
 wv = []
@@ -54,34 +51,29 @@ for i in range(wrong,a.__len__()):
     answer_begin = a[i]['answer_begin_position ']
     answer_end = a[i]['answer_end_position']
     sentence_answer = make_sentence_answer(article_id, answer_begin)
-    for j in question[i]:
-        if j == 'ใคร':
-            # print(i,answer,question[i])
-            # print(sentence_answer)
-            n+=1
-            ans.append([i,answer])
-            s_ans.append(sentence_answer)
-            wv.append([])
-            for k in sentence_answer:
-                if (k in answer) and (k.__len__() >= 2):
-                    wv[-1].append(k)
 
-            break
+    if not answer.isnumeric() and 'ใคร' not in question[i] and 'ว่า' in question[i]  and 'อะไร' in question[i]:
+        # print(i,answer,question[i])
+        # print(sentence_answer)
+        n+=1
+        ans.append([i, answer])
+        s_ans.append(sentence_answer)
+        wv.append([])
+        for j in sentence_answer:
+            if (j in answer) and (j.__len__() >= 2):
+                wv[-1].append(j)
+
 print(n)
 
 string = ''
 for i in range(ans.__len__()):
     if wv[i].__len__() < 1 :
-        print(wv[i])
-        word = []
         for j in s_ans[i]:
-            d = similar(ans[i][1], j)
-            if d > 0.5 :
-                word.append([j, d])
-                string += j + '\n'
-        word.sort(key=lambda s: s[1], reverse=True)
-        print(ans[i], word)
-
-
-with open("train_set_for_classify/human_name_no_wv.txt", "w", encoding="utf-8") as text_file:
-    text_file.write(string)
+            d = similar(j,ans[i][1])
+            if d > 0.45:
+                wv[i].append(j)
+#
+        print(ans[i],wv[i],s_ans[i])
+        string += str(wv[i]) + '\n'
+# with open("train_set_for_classify/something_name_no_wv.txt", "w", encoding="utf-8") as text_file:
+#     text_file.write(string)
