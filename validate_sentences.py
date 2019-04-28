8a58da53fac2a0a51296c334c6a71857cdd9424e
winner <wachirawit.kan@gmail.com>
2019-04-28 10:30:20 +0700

    make new validate sentences

--- validate_sentences.py
+++ validate_sentences.py
@@ -1,8 +1,10 @@
 import json
 
+
 def similar(a, b):
     from difflib import SequenceMatcher
     return SequenceMatcher(None, a, b).ratio()
+
 
 def sentence_similar(a, b):
     a = set(a)
@@ -11,31 +13,36 @@ def sentence_similar(a, b):
     return a.intersection(b).__len__() / a.union(b).__len__()
 
 
-def make_validate_sentences(validate_doc, validate_answer):
+def make_validate_sentences(validate_doc, validate_answer, answer_postition):
     path = 'E:\\CPE#Y4\\databaseTF\\new-documents-tokenize\\'
     validate_sentences = []
     for i in range(validate_doc.__len__()):
         validate_sentences.append([])
-        print(i,validate_answer[i],validate_doc[i])
         doc = json.load(open(path + str(validate_doc[i]) + '.json', mode='r', encoding="utf-8-sig"))[1]
         # print(doc)
-        for j in range(0,doc.__len__(), 10):
-            if j+20 <= doc.__len__():
-                tmp = doc[j:j+20]
+        print(i, validate_answer[i])
+        pos = 0
+        answer_idx = 0
+        for j in range(0, doc.__len__(), 1):
+            pos += doc[j].__len__()
+            if pos > answer_postition[i][0]:
+                answer_idx = j
+                break
+        for j in range(0, doc.__len__(), 10):
+            if j + 20 <= doc.__len__():
+                tmp = doc[j:j + 20]
             else:
-                tmp = doc[doc.__len__()-20:]
+                tmp = doc[doc.__len__() - 20:]
 
-            for k in tmp:
-                if similar(validate_answer[i], k) > 0.4:
-                    validate_sentences[-1].append(tmp)
-                    break
-                elif k != ' ' and k in validate_answer[i]:
-                    validate_sentences[-1].append(tmp)
-                    break
-            if validate_sentences[-1].__len__() > 1:
+            if j <= answer_idx < j + 20:
+                validate_sentences[-1].append(tmp)
+            if validate_sentences[-1].__len__() > 2:
+                print(validate_sentences[-1])
                 break
         # print(validate_sentences[-1])
+        # print(validate_sentences[-1])
         if validate_sentences[-1].__len__() < 1:
+            print("ERROR no validate sentence")
             print(doc)
             break
     with open('test_set\\validate_sentences.json', 'w', encoding="utf-8") as outfile:
@@ -46,10 +53,11 @@ validate_doc = json.load(open("test_set\\new_sample_questions_answer.json", mode
 q = json.load(open('test_set\\no_space_questions_tokenize.json', mode='r', encoding="utf-8-sig"))
 
 validate_answer = []
+answer_postition = json.load(open('test_set/new_answer_positions.json', mode='r', encoding="utf-8-sig"))
+print(answer_postition.__len__())
 data = json.load(open('test_set/new_sample_questions.json', mode='r', encoding="utf-8-sig"))
 for i in data['data']:
     validate_answer.append(i['answer'])
 # print(validate_answer)
 
-make_validate_sentences(validate_doc, validate_answer)
-
+make_validate_sentences(validate_doc, validate_answer, answer_postition)
